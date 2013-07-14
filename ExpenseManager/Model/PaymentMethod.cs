616dcd0bcb1874011317b48769b0b4e14ce54195
André Silva <2493377+askpt@users.noreply.github.com>
2013-07-14 17:57:04 +0100

    Created the base class for the payment method and created the UI for the same function ref #3

---
+++ ExpenseManager/Model/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Model
+{
+    /// <summary>
+    /// The abstract payment method class
+    /// </summary>
+    public abstract class PaymentMethod
+    {
+    }
+}
--- ExpenseManager/View/RegisterPaymentMethodUI.cs
+++ ExpenseManager/View/RegisterPaymentMethodUI.cs
@@ -2,44 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ExpenseManager.View
 {
     /// <summary>
-    /// The main menu of the program
+    /// The specific register payment method ui
     /// </summary>
-    public class ExpenseManagerUI
+    public class RegisterPaymentMethodUI
     {
         /// <summary>
-        /// The method where we will choose the correct option to go
+        /// The specific method to the register payment method ui
         /// </summary>
         public void Show()
         {
-            int option;
-            const int EXIT = 0, REGISTER_EXPENSE = 1, REGISTER_EXPENSE_TYPE = 2, REGISTER_PAYMENT_METHOD = 3;
+            int option = 999;
+            const int EXIT = 0, CREDIT_CARD = 1, DEBIT_CARD = 2, CHEQUE = 3, MONEY = 4;
             do
             {
                 Menu();
                 Console.WriteLine("Choose an option.");
-                
+
                 if (int.TryParse(Console.ReadLine(), out option))
                 {
                     switch (option)
                     {
-                        case REGISTER_EXPENSE:
+                        case CREDIT_CARD:
                             //TODO some code
                             break;
-                        case REGISTER_EXPENSE_TYPE:
-                            RegisterExpenseTypeUI retUI = new RegisterExpenseTypeUI();
-                            retUI.Show();
+                        case DEBIT_CARD:
+                            //TODO some code
                             break;
-                        case REGISTER_PAYMENT_METHOD:
-                            RegisterPaymentMethodUI metUI = new RegisterPaymentMethodUI();
-                            metUI.Show();
+                        case CHEQUE:
+                            //TODO some code
+                            break;
+                        case MONEY:
+                            //TODO some code
                             break;
                         case EXIT:
-                            Console.WriteLine("Bye! Bye!");
+                            Console.WriteLine("Exiting");
                             break;
                         default:
                             Console.WriteLine("Unknown option\nPlease try again!");
@@ -52,15 +52,17 @@ namespace ExpenseManager.View
                 }
             } while (option != 0);
         }
+
         /// <summary>
         /// Shows the main menu
         /// </summary>
         private void Menu()
         {
-            Console.WriteLine(" === Main Menu ===");
-            Console.WriteLine("1. Register Expense");
-            Console.WriteLine("2. Register Expense Type");
-            Console.WriteLine("3. Register Payment Method");
+            Console.WriteLine(" === Register Payment Method Menu ===");
+            Console.WriteLine("1. Credit Card");
+            Console.WriteLine("2. Debit Card");
+            Console.WriteLine("3. Cheque");
+            Console.WriteLine("4. Money");
 
             Console.WriteLine("0. Exit\n");
         }

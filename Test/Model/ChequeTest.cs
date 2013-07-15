147df9d0d2add0f9d3ceb9f4c7c359dd1b2515c3
André Silva <2493377+askpt@users.noreply.github.com>
2013-07-15 18:38:16 +0100

    Added the tests to the payment methods ref #3

---
+++ Test/Model/ChequeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseManager.Model;
+
+namespace Test.Model
+{
+    /// <summary>
+    /// The cheque test class
+    /// </summary>
+    [TestClass]
+    public class ChequeTest
+    {
+        /// <summary>
+        /// Test if the to string method is correct
+        /// </summary>
+        [TestMethod]
+        public void TestToString()
+        {
+            Cheque cq1 = new Cheque("ABC", "AAA", 15645131);
+            Cheque cq2 = new Cheque("ASW", "ASD", 156465);
+            
+            Assert.AreEqual("Cheque\nCheque Book Name: ABC\nBank: AAA\nAccount Number: 15645131", cq1.ToString());
+            Assert.AreEqual("Cheque\nCheque Book Name: ASW\nBank: ASD\nAccount Number: 156465", cq2.ToString());
+        }
+    }
+}
---
+++ Test/Model/DebitCardTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseManager.Model;
+
+namespace Test.Model
+{
+    /// <summary>
+    /// The debit card test class
+    /// </summary>
+    [TestClass]
+    public class DebitCardTest
+    {
+        /// <summary>
+        /// Test if the to string method is correct
+        /// </summary>
+        [TestMethod]
+        public void TestToString()
+        {
+            DebitCard dc1 = new DebitCard("AAA", "123A", "Me", new DateTime(2013, 07, 1), 157568);
+            DebitCard dc2 = new DebitCard("BASD", "qwwe", "You", new DateTime(4000, 12, 7), 16416);
+
+            Assert.AreEqual("Debit Card\nCard Name: AAA\nCard Number: 157568\nOwner: Me\nBank: 123A\nValidity: 2013/7", dc1.ToString());
+            Assert.AreEqual("Debit Card\nCard Name: BASD\nCard Number: 16416\nOwner: You\nBank: qwwe\nValidity: 4000/12", dc2.ToString());
+        }
+    }
+}

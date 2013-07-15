eadd9cd1b2dd0e35ee804013b2d4f838d4a23f6c
André Silva <andresilva15@outlook.com>
2013-07-15 18:38:16 +0100

    Added the tests to the payment methods ref #3

---
+++ Test/Model/CreditCardTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseManager.Model;
+
+namespace Test.Model
+{
+    /// <summary>
+    /// The credit card test class
+    /// </summary>
+    [TestClass]
+    public class CreditCardTest
+    {
+        /// <summary>
+        /// Test if the to string method is correct
+        /// </summary>
+        [TestMethod]
+        public void TestToString()
+        {
+            CreditCard cc1 = new CreditCard("AA", "hi", "Me", new DateTime(2012, 12, 1), 1165131, 15);
+            CreditCard cc2 = new CreditCard("BB", "oi", "you", new DateTime(2013, 09, 1), 156135, 17);
+
+            Assert.AreEqual("Credit Card\nCard Name: AA\nCard Number: 1165131\nOwner: Me\nBank: hi\nValidity: 2012/12\nLimit: 15", cc1.ToString());
+            Assert.AreEqual("Credit Card\nCard Name: BB\nCard Number: 156135\nOwner: you\nBank: oi\nValidity: 2013/9\nLimit: 17", cc2.ToString());
+        }
+    }
+}
---
+++ Test/Model/MoneyTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseManager.Model;
+
+namespace Test.Model
+{
+    /// <summary>
+    /// The Money payment method test
+    /// </summary>
+    [TestClass]
+    public class MoneyTest
+    {
+        /// <summary>
+        /// Test if the to string method is correct
+        /// </summary>
+        [TestMethod]
+        public void TestToString()
+        {
+            Money money1 = new Money("EUR");
+            Money money2 = new Money("DLR");
+
+            Assert.AreEqual("Money\nCurrency: EUR", money1.ToString());
+            Assert.AreEqual("Money\nCurrency: DLR", money2.ToString());
+        }
+    }
+}

abe2a56ffc02da1ea5bb8d2ff878b58e31458e71
André Silva <2493377+askpt@users.noreply.github.com>
2013-08-01 23:23:53 +0100

    Added the controller for the expense. Added necessary methods to know how many expense types and payment method's exists ref #1, #2, #3

---
+++ ExpenseManager/ViewModel/ExpenseController.cs
@@ -0,0 +1,33 @@
+﻿using ExpenseManager.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.ViewModel
+{
+    /// <summary>
+    /// The controller class to the expense functions
+    /// </summary>
+    public class ExpenseController
+    {
+        /// <summary>
+        /// The method that will return the number of expense types existing in the repository
+        /// </summary>
+        /// <returns>the number of existing expense types</returns>
+        public int GetExpenseTypeRepositorySize()
+        {
+            return PersistenceFactory.GetFactory().GetRepository().GetExpenseTypeRepository().All().Count;
+        }
+
+        /// <summary>
+        /// The method that will return the number of payment methods existing in the repository
+        /// </summary>
+        /// <returns>the number of existing payment methods</returns>
+        public int GetPaymentMethodRepositorySize()
+        {
+            return PersistenceFactory.GetFactory().GetRepository().GetPaymentMethodRepository().All().Count;
+        }
+    }
+}

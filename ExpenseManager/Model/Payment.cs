738009ac377864618d171dcbbdfd3c886fc6f9e7
André Silva <2493377+askpt@users.noreply.github.com>
2013-08-02 13:04:07 +0100

    Created the payment classes

---
+++ ExpenseManager/Model/Payment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Model
+{
+    /// <summary>
+    /// The class for the payment of the expenses
+    /// </summary>
+    public class Payment
+    {
+        /// <summary>
+        /// The payment method
+        /// </summary>
+        private PaymentMethod method;
+        /// <summary>
+        /// The payment amount
+        /// </summary>
+        private float amount;
+
+        /// <summary>
+        /// The main constructor for the payment
+        /// </summary>
+        /// <param name="method">the payment method</param>
+        /// <param name="amount">the payment amount</param>
+        public Payment(PaymentMethod method, float amount)
+        {
+            this.method = method;
+            this.amount = amount;
+        }
+
+        /// <summary>
+        /// The ToString method that will show the content of the payment
+        /// </summary>
+        /// <returns>the content of payment in this format: Payment: method\nAmount: amount</returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ExpenseManager/Model/PaymentCheque.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Model
+{
+    /// <summary>
+    /// The class that will extends the payment, in case of payment method is a cheque
+    /// </summary>
+    public class PaymentCheque : Payment
+    {
+        /// <summary>
+        /// The cheque number
+        /// </summary>
+        private int chequeNumber;
+
+        /// <summary>
+        /// The main constructor for the cheque payment
+        /// </summary>
+        /// <param name="method">the payment method</param>
+        /// <param name="amount">the payment amount</param>
+        /// <param name="chequeNumber">the cheque number</param>
+        public PaymentCheque(PaymentMethod method, float amount, int chequeNumber)
+            : base(method, amount)
+        {
+            this.chequeNumber = chequeNumber;
+        }
+
+        /// <summary>
+        /// The ToString method that will show the content of the cheque payment
+        /// </summary>
+        /// <returns>the content of payment in this format: Payment: method\nAmount: amount\nCheque Number: chequeNumber</returns>
+        public override string ToString()
+        {
+            return base.ToString() + "\nCheque Number: " + chequeNumber;
+        }
+    }
+}

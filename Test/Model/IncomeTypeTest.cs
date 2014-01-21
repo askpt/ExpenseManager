18520ec688e6b2c0cf7e16a46222ab373318e2b2
André Silva <2493377+askpt@users.noreply.github.com>
2014-01-21 11:32:00 +0000

    Finished the tests to the income type model and controller class. close #8

---
+++ Test/Model/IncomeTypeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpenseManager.Model;
+
+namespace Test.Model
+{
+    /// <summary>
+    /// The income type test class
+    /// </summary>
+    [TestClass]
+    public class IncomeTypeTest
+    {
+        /// <summary>
+        /// Test if the to string method is correct
+        /// </summary>
+        [TestMethod]
+        public void TestToString()
+        {
+            IncomeType expType1 = new IncomeType("AAA", "aaa");
+            IncomeType expType2 = new IncomeType("A1B1", "testes");
+
+            Assert.AreEqual("AAA - aaa", expType1.ToString());
+            Assert.AreEqual("A1B1 - testes", expType2.ToString());
+        }
+    }
+}

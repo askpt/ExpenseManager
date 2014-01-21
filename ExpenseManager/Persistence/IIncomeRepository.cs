421a8ca6bd4bbef5b84f3fc13fa8b01afd18b06b
André Silva <2493377+askpt@users.noreply.github.com>
2014-01-21 13:06:59 +0000

    Finished the implementation of persistence of the incomes. ref #9

---
+++ ExpenseManager/Persistence/IIncomeRepository.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Persistence
+{
+    /// <summary>
+    /// The interface for the income repository
+    /// </summary>
+    public interface IIncomeRepository
+    {
+        /// <summary>
+        /// Saves an income object in the repository
+        /// </summary>
+        /// <param name="inc">the object of income</param>
+        void Save(Income inc);
+
+        /// <summary>
+        /// Method to get all of incomes
+        /// </summary>
+        /// <returns>the list of incomes</returns>
+        IList<Income> All();
+    }
+}
